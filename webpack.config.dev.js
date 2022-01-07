691bb7a7155de484ca932997c7888052d8eca1ce
nabilAparicio <orlando.nabil@gmail.com>
2022-01-07 11:29:08 -0500

    deploy

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -14,7 +11,8 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
-    mode: 'production',
+    mode: 'development',
+    watch: false,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -77,13 +75,5 @@ plugins: [
         ]
     }),
     new Dotenv(),
-    new CleanWebpackPlugin(),
-],
-optimization:{
-    minimize: true,
-    minimizer:[
-        new CssMinimizerPlugin(),
-        new TerserPlugin(),
-    ]
-}
+]
 }
